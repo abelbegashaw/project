c8060231dabad5b52f235ad08ed756467ed631b4
abelbegashaw <abelbegashawmekuria@gmail.com>
2026-04-30 07:58:00 +0300

    Delete lib directory

--- lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { type ClassValue, clsx } from 'clsx'
-
-/**
- * Merge className strings with Tailwind CSS classes properly
- * Handles conditional classes and resolves Tailwind conflicts
- */
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs)
-}
